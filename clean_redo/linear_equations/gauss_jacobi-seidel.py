360d3b84d5d838c49f497cc44a74677113431252
Bruno Mendes <brod56@sapo.pt>
2021-01-29 16:16:18 +0000

    Improve Gauss Jacobi

--- clean_redo/linear_equations/gauss_jacobi-seidel.py
+++ clean_redo/linear_equations/gauss_jacobi-seidel.py
@@ -1,40 +1,21 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-def gauss_jacobi(m, res, x1, x2, x3, error): # must be 3x3
-    n_x1 = x1
-    n_x2 = x2
-    n_x3 = x3
-    x1_ger = lambda x2,x3: (res[0][0] - m[0][1]*x2 - m[0][2]*x3)/m[0][0]
-    x2_ger = lambda x1,x3: (res[1][0] - m[1][0]*x1 - m[1][2]*x3)/m[1][1]
-    x3_ger = lambda x1,x2: (res[2][0] - m[2][0]*x1 - m[2][1]*x2)/m[2][2]
-    while (True):
-        n_x1 = x1_ger(x2,x3)
-        n_x2 = x2_ger(x1,x3)
-        n_x3 = x3_ger(x1,x2)
-        if (abs(n_x1 - x1) <= error or abs(n_x2 - x2) <= error or abs(n_x3 - x3) <= error):
-            break
-        x1 = n_x1
-        x2 = n_x2
-        x3 = n_x3
-        
-    return n_x1, n_x2, n_x3
+def gauss_jacobi(m, b, max_iter, g, seidel):
+    if len(m) != len(b) or len(m) != len(m[0]) or len(g) != len(m):
+        return False
 
-def gauss_seidel(m, res, x1, x2, x3, error): # must be 3x3
-    n_x1 = x1
-    n_x2 = x2
-    n_x3 = x3
-    x1_ger = lambda x2,x3: (res[0][0] - m[0][1]*x2 - m[0][2]*x3)/m[0][0]
-    x2_ger = lambda x1,x3: (res[1][0] - m[1][0]*x1 - m[1][2]*x3)/m[1][1]
-    x3_ger = lambda x1,x2: (res[2][0] - m[2][0]*x1 - m[2][1]*x2)/m[2][2]
-    while (True):
-        n_x1 = x1_ger(x2,x3)
-        n_x2 = x2_ger(n_x1,n_x3)
-        n_x3 = x3_ger(n_x1,n_x2)
-        if (abs(n_x1 - x1) <= error or abs(n_x2 - x2) <= error or abs(n_x3 - x3) <= error):
-            break
-        x1 = n_x1
-        x2 = n_x2
-        x3 = n_x3
-        
-    return n_x1, n_x2, n_x3
+    #check dominant diagonal
+    for i in range(len(m)):
+        if m[i][i] <= sum(m[i][j] if j != i else 0 for j in range(len(m[0]))):
+            return False
+
+    sols = [0 for _ in range(len(m))]
+    for i in range(max_iter):
+        for l in range(len(m)):
+            sols[l] = (1/m[l][l]) * (b[l] - sum(g[j]*m[l][j] if j!= l else 0 for j in range(len(m[0]))))
+            if seidel: g[l] = sols[l]
+        g = sols.copy()
+        print(g)
+
+    return sols
