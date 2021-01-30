50bfcc67821816eae6572f7764aecf8162bb3bc4
Bruno Mendes <brod56@sapo.pt>
2021-01-30 19:03:50 +0000

    Improve gradient

--- clean_redo/multidimensional_optimization/multi_optimization.py
+++ clean_redo/multidimensional_optimization/multi_optimization.py
@@ -9,17 +9,17 @@ def min_gradient(f, gradient, p, h, error):
         ng = p.copy()
         for i in range(len(p)):
             ng[i] -= h*gradient[i](*p)
-        if any(abs(ng[i] - p[i]) <= error for i in range(len(p))): break
-        if f(*ng) >= f(*p):
+        if any(abs(ng[i] - p[i]) <= error for i in range(len(p))):
+            break
+        elif f(*ng) >= f(*p):
             h = h/2
         else:
             h *= 2
             p = ng.copy()
-        print(p)
-
     return p, count
 
-f = lambda x,y: (x+2)**2 + (y+2)**2 - 5
-gradient = [lambda x,y: 2*(x+2), lambda x,y: 2*(y+2)]
-p = [138467, -2846556]
-print(min_gradient(f, gradient, p, 1, 0.01))
+if __name__ == '__main__':
+    f = lambda x,y: (x+2)**2 + (y+2)**2 - 5
+    gradient = [lambda x,y: 2*(x+2), lambda x,y: 2*(y+2)]
+    p = [138467, -2846556]
+    print(min_gradient(f, gradient, p, 1, 0.00001))
